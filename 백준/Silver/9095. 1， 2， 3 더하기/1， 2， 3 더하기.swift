13e2516a4f359e3b000c0e08815a55ec7315e370
석지한 <sozohoy@gmail.com>
2024-01-09 23:45:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -1,10 +1,6 @@
-var arr = Array(repeating: 0, count: 11)
-arr[0] = 0
-arr[1] = 1
-arr[2] = 2
-arr[3] = 4
-for i in 4..<arr.count {
-    arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3]
+var arr = [0, 1, 2, 4]
+for i in 4..<11 {
+    arr.append(arr[i - 1] + arr[i - 2] + arr[i - 3])
 }
 let input = Int(readLine()!)!
 for _ in 0..<input {
