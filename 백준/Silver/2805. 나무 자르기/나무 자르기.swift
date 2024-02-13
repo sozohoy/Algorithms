ea4cbc1a4bb53122fbdb82277d39ab0176b174cf
한지석 Koji <sozohoy@gmail.com>
2024-02-13 14:39:40 +0900

    [Silver II] Title: 나무 자르기, Time: 572 ms, Memory: 145408 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.swift
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.swift
@@ -1,26 +1,23 @@
-import Foundation
-
-let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let input = readLine()!.split(separator: " ").map { Int($0)! }
 let n = input[0], m = input[1]
-let array = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
-
+var arr = readLine()!.split(separator: " ").map { Int($0)! }.sorted { $0 < $1 }
 var start = 0
-var end = array.last!
+var end = arr.last!
 var result = 0
 
 while start <= end {
     let mid = (start + end) / 2
     var count = 0
-    for i in array {
-        if i > mid {
-            count += (i - mid)
+    for i in 0..<arr.count {
+        if arr[i] > mid {
+            count += (arr[i] - mid)
         }
     }
     if count < m {
         end = mid - 1
     } else {
-        result = mid
         start = mid + 1
+        result = mid
     }
 }
 
