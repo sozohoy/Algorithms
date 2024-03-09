53f8e2acf72c15271baa0f04ab5674d504c90bcd
한지석 Koji <sozohoy@gmail.com>
2024-03-09 23:07:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ number:String, _ k:Int) -> String {
+    let numbers: [String] = number.map { String($0) }
+    var resultStringCount = numbers.count - k 
+    var result = ""
+    if number.count == k {
+        return number
+    }
+    var checkIndex = 0
+    var index = 0
+    for i in stride(from: resultStringCount, to: 0, by: -1) {
+        var maxCount = 0
+        for j in checkIndex...numbers.count - i {
+            if Int(numbers[j])! > maxCount {
+                maxCount = Int(numbers[j])!
+                index = j
+            }
+            if maxCount == 9 {  
+                break    
+            }
+        }
+        result += "\(maxCount)"
+        
+        checkIndex = index + 1
+    }
+    return result
+}
