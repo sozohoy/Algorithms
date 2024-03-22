70de0eb666ad529f8e0ada61a667750302b39283
한지석 Koji <sozohoy@gmail.com>
2024-03-22 19:59:48 +0900

    [level 3] Title: 기지국 설치, Time: 0.59 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ n:Int, _ stations:[Int], _ w:Int) -> Int {
+    var count = 0
+    var length = 0
+    var idx = 0
+    let maxDist = (2*w) + 1
+    
+    while length < n {
+        if idx >= stations.count || (stations[idx] - w ) - length > 1  {
+            length += maxDist
+            count += 1
+        } else {
+            length = stations[idx]+w
+            idx += 1
+        }
+    }
+    return count
+}
